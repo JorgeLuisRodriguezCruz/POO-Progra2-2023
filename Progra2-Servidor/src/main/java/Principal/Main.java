8e27f9580494e35063d5ec60c7544997bebd3458
JorgeLuisRodriguezCruz <rodriguezcruz088@gmail.com>
2023-04-29 14:45:36 -0600

    Se crean las aplicaciones que representaran los módulos  necesarios.

---
+++ Progra2-Servidor/src/main/java/Principal/Main.java
@@ -0,0 +1,13 @@
+package Principal;
+
+/*
+ *
+ * @author rodri
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Progra2-Simulacion/src/main/java/Principal/Main.java
@@ -0,0 +1,14 @@
+package Principal;
+
+/*
+ *
+ * @author rodri
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+}
